f9f94ca2df960f2515dc4a72d6645b8cb3a06072
Pasha127 <pasha.account@gmail.com>
2022-10-24 11:39:02 +0200

    tweaks

--- src/api/me/model.js
+++ src/api/me/model.js
@@ -6,11 +6,11 @@ const userDbSchema = new Schema(
     {
         name: {type: String, required: true},
         surname: {type: String, required: true},
-        email: {type: email, required: true},
+        email: {type: String, required: true},
         bio: {type: String, required: true},
         title: {type: String, required: true},
         area: {type: String, required: true},
-         image: {type: String, required: true},
+        image: {type: String, required: true},
         username: {type: String, required: true}, 
     },
     {timestamps: true}
