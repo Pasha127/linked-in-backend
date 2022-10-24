cecb677e8b9046ed4ffcf74d36df40e192919805
Hatem Mahmoud <hatemhindawi1999@gmail.com>
2022-10-24 11:21:25 +0200

    add schema

--- src/api/me/model.js
+++ src/api/me/model.js
@@ -1,9 +1,17 @@
+import e from "cors";
 import {Schema, model} from "mongoose";
 
 
 const userDbSchema = new Schema(
     {
-      //Schema
+        name: {type: String, required: true},
+        surname: {type: String, required: true},
+        email: {type: email, required: true},
+        bio: {type: String, required: true},
+        title: {type: String, required: true},
+        area: {type: String, required: true},
+         image: {type: String, required: true},
+        username: {type: String, required: true}, 
     },
     {timestamps: true}
   )
